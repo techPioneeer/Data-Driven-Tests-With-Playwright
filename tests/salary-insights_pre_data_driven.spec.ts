e36dcdd655e19c54121d72ec18dbfe12fb36a437
H.S <40137734+techPioneeer@users.noreply.github.com>
2024-11-26 16:54:30 -0500

    postman integration

--- tests/salary-insights_pre_data_driven.spec.ts
+++ tests/salary-insights_pre_data_driven.spec.ts
@@ -1,16 +1,69 @@
 import { test, expect } from '@playwright/test';
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
+import axios from 'axios';
 
 // Define the type for the test data
 interface TestData {
-    role: string;
-    seniorty: string;
-    country: string;
-    currency: string;
+  role: string;
+  seniorty: string;
+  country: string;
+  currency: string;
 
 }
 
+async function fetchTestData(): Promise<TestData[]> {
+  const baseUrl = 'https://8e461e41-9dbd-412d-b57b-1b18cd66eb8e.mock.pstmn.io'
+  const response = await axios.get(`${baseUrl}/test-data/salary-insights-data`);
+  return response.data;
+}
+
+test.describe('Dynamic Salary Insights Tests', () => {
+  let testData: TestData[];
+
+  test.beforeAll(async () => {
+    testData = await fetchTestData();
+
+  });
+  test('Run dynamic tests for all roles', async ({ page }) => {
+    for (const { role, seniorty, country, currency } of testData) {
+        await test.step(`Should display correct compensation info for ${role} in ${seniorty} in ${country} in ${currency}`, async () => {
+        await page.goto("https://www.deel.com/pt/salary-insights");
+
+        // Select Role
+
+        await page.locator('#idIframe').contentFrame().getByPlaceholder('Select a Role *').fill(role);
+        await page.locator('#idIframe').contentFrame().getByRole('listbox', { name: 'Role *' }).click();
+
+        // Select Level
+
+        // await page.locator('#idIframe').contentFrame().getByLabel('Seniorty Level *').click();
+        await page.locator('#idIframe').contentFrame().getByRole('combobox', { name: 'Seniorty Level *' }).fill(seniorty);
+        await page.locator('#idIframe').contentFrame().getByRole('listbox', { name: 'Seniorty Level *' }).click();
+
+
+        // Select Country   
+
+        await page.locator('#idIframe').contentFrame().getByPlaceholder('Country *').fill(country);
+        await page.locator('#idIframe').contentFrame().getByRole('listbox', { name: 'Country *' }).locator('div').nth(3).click();
+
+        // Select Currency
+
+        await page.locator('#idIframe').contentFrame().getByRole('combobox', { name: 'Currency' }).fill(currency);
+        await page.locator('#idIframe').contentFrame().getByRole('listbox', { name: 'Currency' }).locator('div').nth(1).click();
+        // getByText('United States').click();
+
+        // Click Search
+        await page.locator('#idIframe').contentFrame().getByRole('button', { name: 'Search', exact: true }).click();
+
+      });
+
+
+    }
+  });
+});
+
+/*
 // Load test data from JSON file
 const testDataPath = path.resolve(__dirname, 'data', './salary_insights.json');
 let salaryTestData: TestData[];
@@ -62,4 +115,5 @@ test.describe('Salary Insights Tests Naive', () => {
     });
 
 });
+*/
 
